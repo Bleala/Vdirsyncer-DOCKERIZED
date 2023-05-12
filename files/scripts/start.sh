0451153cff9c6822198b0f682f7c5da0ef5d47c0
Bleala <51357182+Bleala@users.noreply.github.com>
2023-05-12 11:20:07 +0200

    perf: cronjobs

--- files/scripts/start.sh
+++ files/scripts/start.sh
@@ -125,23 +125,44 @@ fi
 # Append to crontab file if autodiscover and autosync are true
 if [[ "${AUTODISCOVER}" == "true" ]] && [[ "${AUTOSYNC}" == "true" ]]
 then
-    echo "${CRON_TIME} yes | vdirsyncer -c ${VDIRSYNCER_CONFIG} discover && vdirsyncer -c ${VDIRSYNCER_CONFIG} metasync && vdirsyncer -c ${VDIRSYNCER_CONFIG} sync" >> /etc/crontabs/"${VDIRSYNCER_USER}" 
+    # Write cronjob to file
+    echo "${CRON_TIME} yes | /home/${VDIRSYNCER_USER}/.local/bin/vdirsyncer -c ${VDIRSYNCER_CONFIG} discover \
+    && /home/${VDIRSYNCER_USER}/.local/bin/vdirsyncer -c ${VDIRSYNCER_CONFIG} metasync \
+    && /home/${VDIRSYNCER_USER}/.local/bin/vdirsyncer -c ${VDIRSYNCER_CONFIG} sync" >> "${CRON_FILE}"
+
+    # User info
     echo 'Autodiscover and Autosync are enabled.' 2>&1 | ts '[[%Y-%m-%d %H:%M:%S]]' | tee -a "${LOG}"
-    exec crond -l 8 -d 8 -f 2>&1 | ts '[[%Y-%m-%d %H:%M:%S]]' | tee -a "${LOG}"
+
+    # Start the cronjob
+    /usr/bin/supercronic "${CRON_FILE}" | ts '[[%Y-%m-%d %H:%M:%S]]' | tee -a "${LOG}"
+    #exec crond -l 8 -d 8 -f 2>&1 | ts '[[%Y-%m-%d %H:%M:%S]]' | tee -a "${LOG}"
 
 # Append to crontab file if autosync is true
 elif [[ "${AUTODISCOVER}" == "false" ]] && [[ "${AUTOSYNC}" == "true" ]]
 then
-    echo "${CRON_TIME} vdirsyncer -c ${VDIRSYNCER_CONFIG} metasync && vdirsyncer -c ${VDIRSYNCER_CONFIG} sync" >> /etc/crontabs/"${VDIRSYNCER_USER}"
+    # Write cronjob to file
+    echo "${CRON_TIME} /home/${VDIRSYNCER_USER}/.local/bin/vdirsyncer -c ${VDIRSYNCER_CONFIG} metasync \
+    && /home/${VDIRSYNCER_USER}/.local/bin/vdirsyncer -c ${VDIRSYNCER_CONFIG} sync" >> "${CRON_FILE}"
+
+    # User info
     echo 'Only Autosync is enabled.' 2>&1 | ts '[[%Y-%m-%d %H:%M:%S]]' | tee -a "${LOG}"
-    exec crond -l 8 -d 8 -f 2>&1 | ts '[[%Y-%m-%d %H:%M:%S]]' | tee -a "${LOG}"
+
+    # Start the cronjob
+    /usr/bin/supercronic "${CRON_FILE}" | ts '[[%Y-%m-%d %H:%M:%S]]' | tee -a "${LOG}"
+    #exec crond -l 8 -d 8 -f 2>&1 | ts '[[%Y-%m-%d %H:%M:%S]]' | tee -a "${LOG}"
 
 # Append to crontab file if autodiscover is true
 elif [[ "${AUTODISCOVER}" == "true" ]] && [[ "${AUTOSYNC}" == "false" ]]
 then
-    echo "${CRON_TIME} yes | vdirsyncer -c ${VDIRSYNCER_CONFIG} discover" >> /etc/crontabs/"${VDIRSYNCER_USER}"
+    # Write cronjob to file
+    echo "${CRON_TIME} yes | /home/${VDIRSYNCER_USER}/.local/bin/vdirsyncer -c ${VDIRSYNCER_CONFIG} discover" >> "${CRON_FILE}"
+
+    # User info
     echo 'Only Autodiscover is enabled.' 2>&1 | ts '[[%Y-%m-%d %H:%M:%S]]' | tee -a "${LOG}"
-    exec crond -l 8 -d 8 -f 2>&1 | ts '[[%Y-%m-%d %H:%M:%S]]' | tee -a "${LOG}"
+
+    # Start the cronjob
+    /usr/bin/supercronic "${CRON_FILE}" | ts '[[%Y-%m-%d %H:%M:%S]]' | tee -a "${LOG}"
+    #exec crond -l 8 -d 8 -f 2>&1 | ts '[[%Y-%m-%d %H:%M:%S]]' | tee -a "${LOG}"
 
 # Append nothing, if both options are disabled
 else
